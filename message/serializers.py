c42603d2a19dfea8712cc6dd66516919c5b6618a
Farhanuddin Kazi <kazifarhan199@gmail.com>
2022-11-10 13:05:33 -0600

    feature: Added messages list feature

--- message/serializers.py
+++ message/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from .models import Message
 
 class MessageSerializer(serializers.ModelSerializer):
+    user_username = serializers.SerializerMethodField()
+    to_username = serializers.SerializerMethodField()
+
+    def get_user_username(self, obj):
+        return obj.user.username
+    
+    def get_to_username(self, obj):
+        return obj.to.username
 
     class Meta:
         model = Message
--- message/views.py
+++ message/views.py
@@ -1,6 +1,26 @@
 from .serializers import MessageSerializer
 from .models import Message
 from rest_framework import generics
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 class MessageCreateView(generics.CreateAPIView):
     serializer_class = MessageSerializer
+
+class MessageListView(generics.ListAPIView):
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        queryset = Message.objects.all().order_by('-id')
+        
+        u = self.request.query_params.get('u', None)
+        if u is not None:
+            queryset =queryset.filter(user__username__icontains=u)
+            
+        q = self.request.query_params.get('q', None)
+        if q is not None:
+            queryset =queryset.filter(text__icontains=q)
+        
+        return queryset
